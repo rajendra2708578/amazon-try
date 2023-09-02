89f3fea73cd4a3aa6f6b83ec46c3b3678a3061d5
Amazon-project <nathrajen2710@gmail.com>
2023-09-02 22:13:41 +0530

    make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -84,7 +84,12 @@ forEach((button)=>{
       });
     }
 
+    let cartQuantity=0;
+    cart.forEach((item)=>{
+      cartQuantity+=item.quantity;
+    });
+  
+    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
     
-    console.log(cart);
   });
 });
